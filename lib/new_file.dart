19d40d9336d182003e709f31816c6c74bda79960
Dharmendra Pandey <dharmendrapandey71@gmail.com>
2023-04-23 17:22:45 +0530

    no changes

---
+++ lib/new_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewFile extends StatefulWidget {
+  const NewFile({Key? key}) : super(key: key);
+
+  @override
+  State<NewFile> createState() => _NewFileState();
+}
+
+class _NewFileState extends State<NewFile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
